4118af44e7f3cef5eb9a2d707210473a4e90e2a3
vipulcha <76212979+vipulcha@users.noreply.github.com>
2021-12-12 19:17:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,17 @@
 const express= require("express");
 const bodyParser= require("body-parser");
 const date= require(__dirname + "/date.js");
-
+const {google} = require('googleapis');
+const{OAuth2} = google.auth;
 var ejs = require('ejs');
 var http = require('http');
+const main= require(__dirname + "/main.js");
 
 
 const app= express();
 const items= [];
+let time=[];
+
 
 app.set('view engine', 'ejs');
 
@@ -23,19 +27,51 @@ app.get("/", function(req, res){
 
 app.get("/dashboard", function(req, res){
     let day= date.getDate();
-    res.render("list", {listTitle: day, newListItems: items});
+    
+    res.render("list", {
+        listTitle: day, 
+        newListItems: items,
+        Minutes: time,
+    });
 });
 
 app.post("/dashboard", function(req, res){
 
     let item= req.body.newItem;
-    items.push(item);
+    if(item!=" "){
+        items.push(item);
+        
+    }
+       
     res.redirect("/dashboard");
-
-   /* res.render("list", {listTitle: day, newListItems: items}); */
 });
+
+app.post("/dashboard", function(req, res){
+
+    
+        let min = req.body.userminutes;
+    let sec=req.body.userseconds;
+
+    
+
+
+    time[0]=min;
+    time[1]=sec;
+    res.redirect("/dashboard");
+});
+
 
 
 app.listen(3000, function(){
         console.log("Server started on port 3000");
 });
+
+
+
+
+
+
+
+
+
+
